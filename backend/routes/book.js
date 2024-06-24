89d0f49af5f47ffc5ae119815ee00bb0f3b7c953
Sahil1618 <ss8876584@gmail.com>
2024-06-24 16:47:53 +0530

    completed backend routes

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -28,6 +28,78 @@ router.post("/add-book", authenticateToken, async(req,res)=>{
     }
 })
 
+// update book 
+router.put("/update-book", authenticateToken, async(req,res)=>{
+    try {
+        const { bookid } = req.headers;
+        await Book.findByIdAndUpdate(bookid, {
+            url: req.body.url,
+            title: req.body.title,
+            author: req.body.author,
+            price: req.body.price,
+            desc: req.body.desc,
+            language: req.body.language
+        });
+        return res.status(200).json({message: "Book Updated Successfully."})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "An error occurred" });
+    }
+});
 
+// delete-book -- admin
+router.delete("/delete-book", authenticateToken, async(req,res)=>{
+    try {
+        const { bookid } = req.headers;
+        await Book.findByIdAndDelete(bookid);
+        return res.status(200).json({message: "Book Deleted Successfully."})
+        } 
+    catch (error) {
+        res.status(500).json({ message: "An error occurred" });
+    }
+});
+
+//get all books
+router.get("/get-all-books", async(req,res)=>{
+    try {
+        const books = await Book.find().sort({createdAt: -1});
+        return res.json({
+            status: "Success",
+            data: books,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message : "An error occurred."});
+    }
+});
+
+//get recent books
+router.get("/get-recent-books", async(req,res)=>{
+    try {
+        const books = await Book.find().sort({createdAt: -1}).limit(4);
+        return res.json({
+            status: "Success",
+            data: books,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message : "An error occurred."});
+    }
+});
+
+//get book by id
+router.get("/get-book-by-id/:id", async(req,res)=>{
+    try {
+        const { id } = req.params;
+        const book = await Book.findById(id);
+        return res.json({
+            status: "Success",
+            data: book,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message : "An error occurred."});
+    }
+});
 
 module.exports = router;
