62ff9085c291a62693c90e4b82ad5f6d974b8093
Sahil1618 <ss8876584@gmail.com>
2024-06-26 16:57:11 +0530

    frontend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const app = express();
-app.use(express.json());
+const cors = require("cors");
 require ("dotenv").config();
 require("./conn/conn");
+
 const User = require("./routes/user");
 const Books = require("./routes/book");
 const Favourite = require("./routes/favourite");
 const Cart = require("./routes/cart");
 const Order = require("./routes/order");
+
+app.use(cors());
+app.use(express.json());
 //routes
 app.use("/api/v1", User);
 app.use("/api/v1", Books);
